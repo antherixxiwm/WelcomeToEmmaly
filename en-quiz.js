54112bb00ed2c8e048a734582b0385d01e91a77a
antherixxiwm <adistya.nurani@gmail.com>
2025-05-21 11:52:04 +0700

    Add files via upload

---
+++ en-quiz.js
@@ -0,0 +1,309 @@
+        const questions = [
+            {
+                question: "You are lost in a forest with no equipment. How would you find your way out?",
+                image: "question image/1.png",
+                options: [
+                    { text: "Just charge forward—it'll take me somewhere!", 
+                        value: "Assavadevathin"},
+                    { text: "Look for a river and follow the flow", 
+                        value: "Davichmetha" },
+                    { text: "Follow the guidance of the stars, moon, and sun", 
+                        value: "Meenanagarin" },
+                    { text: "Examine the plants around me and use moss as a navigation", 
+                        value: "Bhuchongpisut" },
+                ]
+            },
+            {
+                question: "It's your birthday, what would you like to receive as a gift?",
+                image: "question image/2.png",
+                options: [
+                    { text: "Functional food or beverage that's good for health",  
+                        value: "Bhuchongpisut" },
+                    { text: "The latest gadget", 
+                        value: "Davichmetha" },
+                    { text: "A unicorn", 
+                        value: "Assavadevathin" },
+                    { text: "A telescope", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "What is your favorite flower?",
+                image: "question image/3.png",
+                options: [
+                    { text: "Jasmine", 
+                        value: "Assavadevathin" },
+                    { text: "Bellflower", 
+                        value: "Davichmetha" },
+                    { text: "Plumeria", 
+                        value: "Assavadevathin" },
+                    { text: "Hortensia", 
+                        value: "Meenanagarin" },
+                    { text: "Rose", 
+                        value: "Bhuchongpisut" },
+                    { text: "Hibiscus", 
+                        value: "Davichmetha" },
+                    { text: "Lily", 
+                        value: "Assavadevathin" },
+                    { text: "I love them all", 
+                        value: "Bhuchongpisut" },
+                    { text: "I'm not that into flowers ...", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "You are going to buy a sofa. What feature would interest you the most?",
+                image: "question image/4.png",
+                options: [
+                    { text: "Leather sofa, cool to the touch", 
+                        value: "Bhuchongpisut" },
+                    { text: "Fabric sofa with smooth texture", 
+                        value: "Assavadevathin" },
+                    { text: "The texture doesn't matter, but the color and design should be good", 
+                        value: "Davichmetha" },
+                    { text: "The outer appearance doesn't matter, but it should be soft and comfortable", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "If you could have one superpower, what would you choose?",
+                image: "question image/5.png",
+                options: [
+                    { text: "Heal any disease", 
+                        value: "Bhuchongpisut" },
+                    { text: "Fly", 
+                        value: "Davichmetha" },
+                    { text: "See clearly in the dark", 
+                        value: "Meenanagarin" },
+                    { text: "Teleportation",
+                        value: "Assavadevathin" },
+                    { text: "Talk to animals", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Who do you trust the most?",
+                image: "question image/6.png",
+                options: [
+                    { text: "Family", 
+                        value: "Meenanagarin" },
+                    { text: "Friends", 
+                        value: "Meenanagarin" },
+                    { text: "Lover", 
+                        value: "Bhuchongpisut" },
+                    { text: "Attendant", 
+                        value: "Assavadevathin" },
+                    { text: "Mentor", 
+                        value: "Davichmetha" },
+                ]
+            },
+            {
+                question: "You are taking a walk at night in Emmaly and someone suspicious is following you. What will you do?",
+                image: "question image/7.png",
+                options: [
+                    { text: "Keep walking—they won't try to harm me and even if they did, I'd be ready", 
+                        value: "Assavadevathin" },
+                    { text: "Confront them directly—I'm ready for every possible outcome", 
+                        value: "Bhuchongpisut" },
+                    { text: "Ask help from the nearest stranger I encounter", 
+                        value: "Meenanagarin" },
+                    { text: "Connect my phone to the nearest speaker and put on alarm sound to chase them off", 
+                        value: "Davichmetha" },
+                    { text: "Go to the nearest night market where it's still crowded and wait until it's safe", 
+                        value: "Davichmetha" },
+                ]
+            },
+            {
+                question: "You feel tired from a long exhausting day. What will you do first?",
+                image: "question image/8.png",
+                options: [
+                    { text: "Go straight to the soft bed", 
+                        value: "Assavadevathin" },
+                    { text: "Prepare a hot bath and soak in", 
+                        value: "Meenanagarin" },
+                    { text: "Go for a relaxing massage", 
+                        value: "Davichmetha" },
+                    { text: "Eat dinner and drink my vitamins and supplements", 
+                        value: "Bhuchongpisut" },
+                ]
+            },
+            {
+                question: "What is your ideal date? (Inspired by Phi Den's tweet :3 )",
+                image: "question image/9.png",
+                options: [
+                    { text: "Do our favorite sport, maybe compete against each other?", 
+                        value: "Davichmetha" },
+                    { text: "Quality time at home, just us both", 
+                        value: "Bhuchongpisut" },
+                    { text: "Visit historical sites and learn more about Emmalian culture", 
+                        value: "Meenanagarin" },
+                    { text: "Horseback riding and have a romantic outdoor picnic", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Your favorite subject?",
+                image: "question image/10.png",
+                options: [
+                    { text: "Math", 
+                        value: "Davichmetha" },
+                    { text: "Chemistry", 
+                        value: "Bhuchongpisut" },
+                    { text: "Physics", 
+                        value: "Davichmetha" },
+                    { text: "Biology", 
+                        value: "Assavadevathin" },
+                    { text: "History", 
+                        value: "Meenanagarin" },
+                    { text: "Sociology", 
+                        value: "Meenanagarin" },
+                    { text: "Geography", 
+                        value: "Bhuchongpisut" },
+                    { text: "Language", 
+                        value: "Meenanagarin" },
+                    { text: "Physical education", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Where do you want to spend your holiday?",
+                image: "question image/11.png",
+                options: [
+                    { text: "In a hut on the beach", 
+                        value: "Meenanagarin" },
+                    { text: "In a cottage on the mountain", 
+                        value: "Bhuchongpisut" },
+                    { text: "In the countryside with all the plantation and farm", 
+                        value: "Assavadevathin" },
+                    { text: "In the city with the varying culinary choice and shopping centers", 
+                        value: "Davichmetha" },
+                ]
+            },
+        ];
+
+        const emmalyHouses = {
+            "Assavadevathin": {
+                name: "Assavadevathin",
+                image: "result/EN Assavadevathin.png",
+                color: "golden" //these "colors" are from Gemini's template, but they aren't actually used
+            },
+            "Bhuchongpisut": {
+                name: "Bhuchongpisut",
+                image: "result/EN Bhuchongpisut.png",
+                color: "green"
+            },
+            "Davichmetha": {
+                name: "Davichmetha",
+                image: "result/EN Davichmetha.png",
+                color: "pink"
+            },
+            "Meenanagarin": {
+                name: "Meenanagarin",
+                image: "result/EN Meenanagarin.png",
+                color: "blue"
+            },
+        };
+
+let currentQuestionIndex = 0;
+let answers = [];
+
+        //const questionContainer = document.getElementById('question-container');
+        //const questionTextElement = document.getElementById('question-text');
+        //const answerOptionsElement = document.getElementById('answer-options');
+        const nextButton = document.getElementById('next-button');
+        const resultsContainer = document.getElementById('results-container');
+        const emmalyHouseResultElement = document.getElementById('emmaly-house-result');
+        const resultsImageElement = document.getElementById('result-image');
+        const restartButton = document.getElementById('restart-button');
+        //const houseBannerElement = document.getElementById('house-banner');
+
+function displayQuestion() {
+    const questionTextElement = document.getElementById('question-text');
+    const questionImageElement = document.getElementById('question-image');
+    const answerOptionsElement = document.getElementById('answer-options');
+    const currentQuestion = questions[currentQuestionIndex];
+    questionTextElement.textContent = currentQuestion.question;
+    questionImageElement.innerHTML = `<img src="${currentQuestion.image}" alt="Question ${currentQuestion + 1}" height="200">`;
+    answerOptionsElement.innerHTML = '';
+
+    currentQuestion.options.forEach(option => {
+        const button = document.createElement('button');
+        button.textContent = option.text;
+        button.className = "btn barlow-medium answer-options-button";
+        button.addEventListener('click', () => handleAnswer(option.value));
+        answerOptionsElement.appendChild(button);
+        });
+
+        nextButton.disabled = true;
+        nextButton.disabled = answers.length <= currentQuestionIndex;
+        nextButton.classList.toggle('opacity-50', nextButton.disabled);
+}
+
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
+    document.getElementById('results-container').style.display = 'none';
+    displayQuestion();
+});
+
+
+function handleAnswer(answer) {
+    answers.push(answer);
+    nextButton.disabled = false;
+    nextButton.classList.remove('opacity-50');
+}
+
+function calculateEmmalyHouse() {
+    let houseCounts = {
+        "Assavadevathin": 0,
+        "Bhuchongpisut": 0,
+        "Davichmetha": 0,
+        "Meenanagarin": 0
+    };
+    answers.forEach(answer => {
+        houseCounts[answer]++;
+    });
+
+    let sortedHouses = Object.entries(houseCounts).sort(([, countA], [, countB]) => countB - countA);
+    return sortedHouses[0][0];
+}
+
+function showResults() {
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'none';
+    document.getElementById('results-container').style.display = 'block';
+
+    const emmalyHouse = calculateEmmalyHouse();
+    const houseData = emmalyHouses[emmalyHouse] || {
+        name: "Unknown",
+        description: "No matching Emmaly house found.",
+        color: ""
+    };
+    emmalyHouseResultElement.textContent = houseData.name;
+    resultsImageElement.innerHTML = `<img src="${houseData.image}" alt="${houseData.name}" width="100%">`;
+    //houseBannerElement.className = `house-banner rounded-t-md ${houseData.color}`;
+    //emmalyHouseResultElement.className = houseData.color; 
+}
+
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        displayQuestion();
+    } else {
+        showResults();
+        }
+    }
+
+function restartTest() {
+    currentQuestionIndex = 0;
+    answers = [];
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
+    document.getElementById('results-container').style.display = 'none';
+    displayQuestion();
+}
+
+nextButton.addEventListener('click', nextQuestion);
+restartButton.addEventListener('click', restartTest);
+
+displayQuestion();
---
+++ id-quiz.js
@@ -0,0 +1,309 @@
+        const questions = [
+            {
+                question: "Kamu tersesat di hutan tanpa peralatan apa pun. Bagaimana caramu mencari jalan keluar?",
+                image: "question image/1.png",
+                options: [
+                    { text: "Maju saja—nanti juga sampai!", 
+                        value: "Assavadevathin"},
+                    { text: "Cari sungai dan ikuti arah arusnya", 
+                        value: "Davichmetha" },
+                    { text: "Mengikuti panduan bintang, bulan, dan matahari", 
+                        value: "Meenanagarin" },
+                    { text: "Memeriksa tanaman di sekelilingku dan menentukan arah dengan bantuan lumut", 
+                        value: "Bhuchongpisut" },
+                ]
+            },
+            {
+                question: "Kamu lagi ulang tahun, paling senang kalau menerima hadiah apa?",
+                image: "question image/2.png",
+                options: [
+                    { text: "Pangan fungsional yang menyehatkan",  
+                        value: "Bhuchongpisut" },
+                    { text: "Gadget terbaru", 
+                        value: "Davichmetha" },
+                    { text: "Seekor unicorn", 
+                        value: "Assavadevathin" },
+                    { text: "Sebuah teleskop", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "Bunga kesukaanmu apa?",
+                image: "question image/3.png",
+                options: [
+                    { text: "Melati", 
+                        value: "Assavadevathin" },
+                    { text: "Bunga lonceng", 
+                        value: "Davichmetha" },
+                    { text: "Kamboja", 
+                        value: "Assavadevathin" },
+                    { text: "Hortensia", 
+                        value: "Meenanagarin" },
+                    { text: "Mawar", 
+                        value: "Bhuchongpisut" },
+                    { text: "Bunga sepatu", 
+                        value: "Davichmetha" },
+                    { text: "Bakung", 
+                        value: "Assavadevathin" },
+                    { text: "Aku cinta semua bunga", 
+                        value: "Bhuchongpisut" },
+                    { text: "Aku tidak terlalu suka bunga ...", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "Kamu akan membeli sofa. Hal apa yang paling membuatmu tertarik?",
+                image: "question image/4.png",
+                options: [
+                    { text: "Sofa kulit, adem kalau dipegang", 
+                        value: "Bhuchongpisut" },
+                    { text: "Sofa kain yang lembut tekstur permukaannya", 
+                        value: "Assavadevathin" },
+                    { text: "Tekturnya tidak masalah, tetapi warna dan desainnya harus bagus", 
+                        value: "Davichmetha" },
+                    { text: "Penampilannya tidak masalah, tetapi harus nyaman dan lembut untuk duduk", 
+                        value: "Meenanagarin" },
+                ]
+            },
+            {
+                question: "Kalau kamu bisa punya satu kekuatan super, kekuatan apa yang akan kamu pilih?",
+                image: "question image/5.png",
+                options: [
+                    { text: "Menyembuhkan segala penyakit", 
+                        value: "Bhuchongpisut" },
+                    { text: "Terbang", 
+                        value: "Davichmetha" },
+                    { text: "Melihat dengan jelas di kegelapan", 
+                        value: "Meenanagarin" },
+                    { text: "Teleportasi",
+                        value: "Assavadevathin" },
+                    { text: "Berbicara dengan hewan", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Siapa yang paling kamu percayai?",
+                image: "question image/6.png",
+                options: [
+                    { text: "Keluarga", 
+                        value: "Meenanagarin" },
+                    { text: "Teman", 
+                        value: "Meenanagarin" },
+                    { text: "Kekasih", 
+                        value: "Bhuchongpisut" },
+                    { text: "Anak buah", 
+                        value: "Assavadevathin" },
+                    { text: "Mentor", 
+                        value: "Davichmetha" },
+                ]
+            },
+            {
+                question: "Kamu sedang jalan-jalan malam di Emmaly, dan ada orang mencurigakan yang mengikutimu. Apa yang akan kamu lakukan?",
+                image: "question image/7.png",
+                options: [
+                    { text: "Tetap jalan terus saja—sepertinya mereka tidak akan mencoba menyakitiku. Tetapi kalaupun iya, aku siap kok", 
+                        value: "Assavadevathin" },
+                    { text: "Konfrontasi langsung terang-terangan—mereka mau ngapain pun aku siap", 
+                        value: "Bhuchongpisut" },
+                    { text: "Meminta tolong pada orang asing yang aku temukan di jalan", 
+                        value: "Meenanagarin" },
+                    { text: "Menyambungkan ponselku ke speaker terdekat dan menyalakan suara alarm untuk mengusir orang itu", 
+                        value: "Davichmetha" },
+                    { text: "Pergi ke pasar malam terdekat yang masih ramai dan menunggu hingga keadaan aman", 
+                        value: "Davichmetha" },
+                ]
+            },
+            {
+                question: "Kamu merasa lelah sekali setelah hari yang panjang. Apa yang kamu lakukan terlebih dahulu?",
+                image: "question image/8.png",
+                options: [
+                    { text: "Langsung ke kasur yang empuk", 
+                        value: "Assavadevathin" },
+                    { text: "Siapkan mandi air hangat dan berendam", 
+                        value: "Meenanagarin" },
+                    { text: "Mampir dulu ke tukang pijat untuk pijat relaksasi", 
+                        value: "Davichmetha" },
+                    { text: "Makan malam lalu minum vitamin dan suplemen", 
+                        value: "Bhuchongpisut" },
+                ]
+            },
+            {
+                question: "Kencan idealmu seperti apa? (Terinspirasi dari tweet-nya P'Den :3 )",
+                image: "question image/9.png",
+                options: [
+                    { text: "Olahraga kesukaan kita, lawan satu sama lain kayaknya asyik juga? P balap", 
+                        value: "Davichmetha" },
+                    { text: "Quality time di rumah, berduaan saja", 
+                        value: "Bhuchongpisut" },
+                    { text: "Mengunjungi situs sejarah dan mempelajari lebih lanjut tentang budaya Emmaly", 
+                        value: "Meenanagarin" },
+                    { text: "Naik kuda dan piknik outdoor romantis", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Mata pelajaran favoritmu?",
+                image: "question image/10.png",
+                options: [
+                    { text: "Matematika", 
+                        value: "Davichmetha" },
+                    { text: "Kimia", 
+                        value: "Bhuchongpisut" },
+                    { text: "Fisika", 
+                        value: "Davichmetha" },
+                    { text: "Biologi", 
+                        value: "Assavadevathin" },
+                    { text: "Sejarah", 
+                        value: "Meenanagarin" },
+                    { text: "Sosiologi", 
+                        value: "Meenanagarin" },
+                    { text: "Geografi", 
+                        value: "Bhuchongpisut" },
+                    { text: "Bahasa", 
+                        value: "Meenanagarin" },
+                    { text: "Penjaskes (pendidikan jasmani, olahraga, dan kesehatan)", 
+                        value: "Assavadevathin" },
+                ]
+            },
+            {
+                question: "Kamu paling ingin liburan di mana?",
+                image: "question image/11.png",
+                options: [
+                    { text: "Gubuk di tepi pantai", 
+                        value: "Meenanagarin" },
+                    { text: "Vila aesthetic di gunung", 
+                        value: "Bhuchongpisut" },
+                    { text: "Di pedesaan yang banyak sawah, kebun, dan peternakannya", 
+                        value: "Assavadevathin" },
+                    { text: "Di perkotaan yang banyak pilihan kuliner dan pusat perbelanjaannya", 
+                        value: "Davichmetha" },
+                ]
+            },
+        ];
+
+        const emmalyHouses = {
+            "Assavadevathin": {
+                name: "Assavadevathin",
+                image: "result/EN Assavadevathin.png",
+                color: "golden" //these "colors" are from Gemini's template, but they aren't actually used
+            },
+            "Bhuchongpisut": {
+                name: "Bhuchongpisut",
+                image: "result/EN Bhuchongpisut.png",
+                color: "green"
+            },
+            "Davichmetha": {
+                name: "Davichmetha",
+                image: "result/EN Davichmetha.png",
+                color: "pink"
+            },
+            "Meenanagarin": {
+                name: "Meenanagarin",
+                image: "result/EN Meenanagarin.png",
+                color: "blue"
+            },
+        };
+
+let currentQuestionIndex = 0;
+let answers = [];
+
+        //const questionContainer = document.getElementById('question-container');
+        //const questionTextElement = document.getElementById('question-text');
+        //const answerOptionsElement = document.getElementById('answer-options');
+        const nextButton = document.getElementById('next-button');
+        const resultsContainer = document.getElementById('results-container');
+        const emmalyHouseResultElement = document.getElementById('emmaly-house-result');
+        const resultsImageElement = document.getElementById('result-image');
+        const restartButton = document.getElementById('restart-button');
+        //const houseBannerElement = document.getElementById('house-banner');
+
+function displayQuestion() {
+    const questionTextElement = document.getElementById('question-text');
+    const questionImageElement = document.getElementById('question-image');
+    const answerOptionsElement = document.getElementById('answer-options');
+    const currentQuestion = questions[currentQuestionIndex];
+    questionTextElement.textContent = currentQuestion.question;
+    questionImageElement.innerHTML = `<img src="${currentQuestion.image}" alt="Pertanyaan ${currentQuestion + 1}" height="200">`;
+    answerOptionsElement.innerHTML = '';
+
+    currentQuestion.options.forEach(option => {
+        const button = document.createElement('button');
+        button.textContent = option.text;
+        button.className = "btn barlow-medium answer-options-button";
+        button.addEventListener('click', () => handleAnswer(option.value));
+        answerOptionsElement.appendChild(button);
+        });
+
+        nextButton.disabled = true;
+        nextButton.disabled = answers.length <= currentQuestionIndex;
+        nextButton.classList.toggle('opacity-50', nextButton.disabled);
+}
+
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
+    document.getElementById('results-container').style.display = 'none';
+    displayQuestion();
+});
+
+
+function handleAnswer(answer) {
+    answers.push(answer);
+    nextButton.disabled = false;
+    nextButton.classList.remove('opacity-50');
+}
+
+function calculateEmmalyHouse() {
+    let houseCounts = {
+        "Assavadevathin": 0,
+        "Bhuchongpisut": 0,
+        "Davichmetha": 0,
+        "Meenanagarin": 0
+    };
+    answers.forEach(answer => {
+        houseCounts[answer]++;
+    });
+
+    let sortedHouses = Object.entries(houseCounts).sort(([, countA], [, countB]) => countB - countA);
+    return sortedHouses[0][0];
+}
+
+function showResults() {
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'none';
+    document.getElementById('results-container').style.display = 'block';
+
+    const emmalyHouse = calculateEmmalyHouse();
+    const houseData = emmalyHouses[emmalyHouse] || {
+        name: "Unknown",
+        description: "No matching Emmaly house found.",
+        color: ""
+    };
+    emmalyHouseResultElement.textContent = houseData.name;
+    resultsImageElement.innerHTML = `<img src="${houseData.image}" alt="${houseData.name}" width="100%">`;
+    //houseBannerElement.className = `house-banner rounded-t-md ${houseData.color}`;
+    //emmalyHouseResultElement.className = houseData.color; 
+}
+
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        displayQuestion();
+    } else {
+        showResults();
+        }
+    }
+
+function restartTest() {
+    currentQuestionIndex = 0;
+    answers = [];
+    document.getElementById('quiz-start-page').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
+    document.getElementById('results-container').style.display = 'none';
+    displayQuestion();
+}
+
+nextButton.addEventListener('click', nextQuestion);
+restartButton.addEventListener('click', restartTest);
+
+displayQuestion();
